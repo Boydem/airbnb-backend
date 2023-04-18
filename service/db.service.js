66a14b6def0898ff9f461572956d52402f0a23b6
Boydem <noyemdahan@gmail.com>
2023-04-18 19:45:14 +0300

    fixing the env vars for mongodb

--- service/db.service.js
+++ service/db.service.js
@@ -22,11 +22,11 @@ async function getCollection(collectionName) {
 async function connect() {
     if (dbConn) return dbConn
     try {
-        const client = await MongoClient.connect(process.env.MY_MONGODB_URL, {
+        const client = await MongoClient.connect(process.env.MY_MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-        const db = client.db(process.env.MY_MONGODB_NAME)
+        const db = client.db(process.env.MY_MONGO_NAME)
         dbConn = db
         return db
     } catch (err) {
