8a438fafa9190dae154d8ae0a242bba83de1a6ad
Boydem <noyemdahan@gmail.com>
2023-04-18 18:49:42 +0300

    updated routes imports

--- api/stay/stay.routes.ts
+++ api/stay/stay.routes.ts
@@ -1,3 +1,4 @@
+const express = require('express')
 const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth.middleware')
 const { log } = require('../../middlewares/logger.middleware')
 const stayController = require('./stay.controller')
